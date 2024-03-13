d89bdf6738e992b81db15f78ee05a5271dceb873
Tabitha Buder <tabibuder@gmail.com>
2024-03-13 01:53:27 -0700

    added functions to fetch both querys and started adding weather display html

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,49 @@ const APIKey = '4115e0c777e4f5d36149f0410dd53abe';
 var historyEl = document.getElementById('searchHistory');
 var longitude;
 var latitude;
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log('button works')
+
+
+  let cityName = document.getElementById('cityInput').value;
+
+
+  localStorage.setItem(cityName,cityName)
+  console.log(cityName)
+
+  getGeoCode(cityName);
+});
+
+function getGeoCode(cityName) {
+  var geoQuery = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=' + 1 + '&appid=' + APIKey + '&units=imperial';
+
+  fetch(geoQuery)
+    .then(function (response) {return response.json()})
+    .then(function(data) {
+      console.log(data);
+      latitude = data[0].lat;
+      longitude = data[0].lon;
+      console.log(latitude,longitude);
+
+      getWeather(latitude, longitude);
+    });
+}
+
+function getWeather(latitude, longitude) {
+  var weatherQuery = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey + '&units=imperial';
+
+  fetch(weatherQuery)
+    .then(function(response) {return response.json()})
+    .then(function(data) {
+      console.log(data);
+      displayWeather(data);
+    }).catch(function(error){
+      console.error('Failed to fetch weather', error);
+    });
+}
+
+function displayWeather(data) {
+  
+}
